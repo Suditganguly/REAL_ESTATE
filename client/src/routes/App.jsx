0b9c4dc810a96b6478e6f36ba4eb9f3aaad64d09
roshni-ghosal <roshnighosal2004@gmail.com>
2025-05-20 00:52:42 +0530

    Made user login api

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -1,18 +1,18 @@
 import { Suspense, useState } from "react";
 import "./App.css";
-import Layout from "./components/Layout/Layout";
+import Layout from "../components/Layout/Layout";
 
-import Website from "./pages/Website";
+import Website from "../pages/Website";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Properties from "./pages/Properties/Properties";
+import Properties from "../pages/Properties/Properties";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Property from "./pages/Property/Property";
-import UserDetailContext from "./context/UserDetailContext";
-import Bookings from "./pages/Bookings/Bookings";
-import Favourites from "./pages/Favourites/Favourites";
+import Property from "../pages/Property/Property";
+import UserDetailContext from "../context/UserDetailContext";
+import Bookings from "../pages/Bookings/Bookings";
+import Favourites from "../pages/Favourites/Favourites";
 
 function App() {
   const queryClient = new QueryClient();
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const dotenv=require('dotenv').config();
+const mongoose= require ('mongoose');
+const dbConnect = require ('./config/db');
+const Userdatabase= require ('./models/userModel')
+const userRoute= require ('./routes/userRoutes')
+const app = express();
+
+// middleware
+app.use(express.json());
+
+//routes
+app.use(`${process.env.USER_ROUTE}`, userRoute);
+
+
+
+// server start
+app.listen(process.env.PORT, ()=>
+    console.log(`this server is running on ${process.env.PORT}`)
+)
