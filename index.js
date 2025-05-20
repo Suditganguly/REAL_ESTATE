01e8aa05b53b4f5312bacda555b6aef82fc63966
Paddy <padmanavo221003003060@technoindiaeducation.com>
2025-05-20 19:39:46 +0530

    backend done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
@@ -11,7 +12,11 @@ const uploadRoutes = require('./routes/upload');
 
 const app = express();
 
+console.log('Upload route loaded');
+
+
 app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -33,7 +38,16 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('MongoDB connection error:', err);
   });
 
+
+ 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+
+ app.use((err, req, res, next) => {
+  console.error('Global error handler:', err.message);
+  res.status(500).json({ error: err.message || 'Internal Server Error' });
 });
